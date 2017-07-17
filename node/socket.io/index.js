9a4e34e635bd977a93f13340c533a366ccfd266f
Administrator <15196004361@163.com>
2017-07-17 14:11:33 +0800

    add my first socket.io app

--- node/socket.io/index.js
+++ node/socket.io/index.js
@@ -6,6 +6,12 @@ app.get('', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 });
 
+io.on('connection', socket => {
+    socket.on('chat message', msg => {
+      io.emit('chat message',msg)
+    })
+});
+
 http.listen(3000, () => {
     console.log('listening on *:3000')
-})
+});
