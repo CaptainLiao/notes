4f7915cd3f524141b852c8efa4379661d22e3e86
Administrator <15196004361@163.com>
2017-08-12 11:35:28 +0800

    使用axios请求数据

--- react-app/antd-demo/antd-demo/src/views/learnCar/test.js
+++ react-app/antd-demo/antd-demo/src/views/learnCar/test.js
@@ -5,17 +5,39 @@ import axios from 'axios';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import './test.less'
 
-
- class Test extends React.Component {
+let api = 'http://jisujiakao.market.alicloudapi.com/driverexam/query';
+class Test extends React.Component {
   state = {
     txt: 'car test 1'
   }
+
   componentDidMount() {
     const { store} = this.context;
     let txt = store.getState().chooseTestLevels;
     txt[0] && this.setState({
       txt: txt[0] + txt[1]
     })
+
+    let subject = 2;
+    if(txt[1] && txt[1].indexOf('一') >= 0) subject = 1;
+    txt[1] && axios({
+      method: 'get',
+      url: api,
+      params: {
+        pagenum: 1,
+        pagesize: 10,
+        sort: 'normal',
+        subject,
+        type: txt[0]
+      },
+      headers:{"Authorization": "APPCODE 8702e06255074019b33bb0a15f75e786"}
+    })
+      .then( (response) => {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
   render() {
 
