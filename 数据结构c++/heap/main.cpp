fa5419f69ba363b0abaab131f425ad6577d07073
Administrator <15196004361@163.com>
2017-11-25 21:51:51 +0800

    add heap

--- 数据结构c++/heap/main.cpp
+++ 数据结构c++/heap/main.cpp
@@ -13,11 +13,20 @@ class MaxHeap{
 	private:
 		Item* data;
 		int count;
+		int capacity;
+		void shiftUp( int k ) {
+			
+			while( k > 1 && data[k/2] < data[k] ) {
+				swap(data[k/2], data[k]);
+				k /= 2;
+			}
+		}
 	
 	public:	
 		MaxHeap(int capacity) {
 			data = new Item[capacity + 1];
 			count = 0;
+			this->capacity = capacity;
 		}
 	
 		~MaxHeap() {
@@ -31,12 +40,27 @@ class MaxHeap{
 		bool isEmpty() {
 			return count == 0;
 		}
+		
+		void insert(Item item) {
+			
+			assert( count + 1 <= capacity);
+			data[++count] = item;
+			shiftUp( count );
+		}
+		
+		Item extractMax() {
+			assert( count > 0 );
+		}
 };
 
 int main() {
 	MaxHeap<int> maxHeap = MaxHeap<int>(100);
 	cout<<maxHeap.size()<<endl;
 	
+	srand(time(NULL));
+	for ( int i = 0; i < 15; i++) {
+		maxheap.insert( rand()%100 );
+	}
 	return 0;
 }
 
