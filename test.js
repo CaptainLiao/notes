f005b4e937ec8df0519288de142f3df88b0c4a32
CaptainLiao <15196004361@163.com>
2018-02-01 11:33:11 +0800

    通用下载页.md

--- test.js
+++ test.js
@@ -7,5 +7,28 @@ function trim(s) {
 assert.equal(trim(' sd  '), 'sd')
 assert.equal(trim(' s d  '), 's d')
 
+function getIn(target, keyPath, notFoundValue = undefined) {
+  for (let key in keyPath) {
+    if (target && target.hasOwnProperty(keyPath[key])) {
+      target = target[keyPath[key]];
+    } else {
+      return notFoundValue;
+    }
+  }
+  return target;
+}
 
+function get(target, keyPathStr, notFoundValue = undefined) {
+  return getIn(target, keyPathStr.split('.'), notFoundValue);
+}
 
+let getTest = {
+  a: {
+    b: [{
+      c: 111
+    },{
+      d: 222
+    }]
+  }
+}
+assert.equal(get(getTest, 'a.b[0].c'), 111)
