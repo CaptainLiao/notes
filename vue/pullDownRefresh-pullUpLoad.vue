860bf4ca385c79ca91ea7b57af957f462e7a7119
CaptainLiao <15196004361@163.com>
2018-07-19 09:40:27 +0800

    优化上拉加载下拉刷新组件

--- vue/pullDownRefresh-pullUpLoad.vue
+++ vue/pullDownRefresh-pullUpLoad.vue
@@ -1,61 +1,79 @@
-<template><!-- 下拉刷新上拉加载 -->
-  <div id="refreshLoad" 
-        @touchstart="touchStart" 
-        @touchmove="touchMove" 
-        @touchend="touchEnd"
-        :style="{'margin-top': pullDownStatus ? downMarginTop+'px' : '', 'transition': pullDownStatus === 'refreshed' ? 'all .3s ease-in': ''}">
-    <div class="down-refresh" :style="{'line-height': height+'px'}">
-      <slot name="down-refresh-status">
-        <image class="img" v-show="pullDownStatus === 'waiting'" v-imagesrc="'delaycare/coin.png'" />
-        <image class="img" v-show="pullDownStatus === 'pending'" v-imagesrc="'delaycare/coin.png'" />
+<!-- 
+下拉刷新上拉加载 
+<scroll-view 
+      :pull-down-status="pullDownStatus" 
+      @scrolltoupper="onStartPullDownRefresh"
+      :lower-threshold="'100'" // 默认50
+      @scrolltolower="onStartPullUpLoad">
+  <div>xxxxxxxxxxxxxxx</div>
+</scroll-view>
+-->
+<template>
+<div id="refreshLoad">
+  <!-- 下拉刷新区 -->
+  <div class="down-refresh" :style="pullDownStyle">
+    <slot name="down-refresh" ref="down-refresh">
+      <image class="img" v-show="pullDownStatus === 'refreshed'" v-imagesrc="'delaycare/coin.png'" />
 
-        <image class="img" v-show="pullDownStatus === 'refreshing'" v-imagesrc="'delaycare/coin.png'" />
-        <image class="rotate-track" v-show="pullDownStatus === 'refreshing'" v-imagesrc="'delaycare/track1.png'" />
-
-        <image class="img" v-show="pullDownStatus === 'refreshed'" v-imagesrc="'delaycare/coin.png'" />
-      </slot>
-    </div>
-    <slot></slot>
-    <div class="up-load" v-show="pullUpStatus">
-      <span v-show="pullUpStatus === 'waiting'">上拉加载更多</span>
-      <span v-show="pullUpStatus === 'loading'">加载中……</span>
-    </div>
+      <image class="img" v-show="pullDownStatus === 'refreshing'" v-imagesrc="'delaycare/coin.png'" />
+      <image class="rotate-track" v-show="pullDownStatus === 'refreshing'" v-imagesrc="'delaycare/track1.png'" />
+    </slot>
   </div>
+
+  <!-- 内容区域 -->
+  <div class="conent-wrapper"
+      @touchstart="touchStart" 
+      @touchmove="touchMove"
+      @touchend="touchEnd">
+    <slot></slot>
+  </div>
+
+  <!-- 上拉加载区 -->
+  <div class="up-load">
+    <slot name="up-load"></slot>
+  </div>
+</div>
+
 </template>
 
 <script>
+// 可自定义
+const MARGIN_TOP = 84
+const SCROLL_ELEM = '.a-layout-body'
 
+const WINDOW_HEIGHT = window.innerHeight
 const SPEED = 0.8
-const MARGIN_TOP = -84
 
-let _speed = SPEED
 let _start_y
 let _last_y
-let _ready_to_refresh
+let _speed = SPEED
+let _pull_down_margin_top = -MARGIN_TOP
+let _scroll_elem
 
 export default {
   data() {
     return {
-      height: -MARGIN_TOP,
-      downMarginTop: MARGIN_TOP,
+      pullDownStyle: `height:${MARGIN_TOP}px;margin-top:${_pull_down_margin_top}px`
     }
   },
 
-  props: {
+  props: { // 由父组件控制状态
     pullDownStatus: {
       type: String,
-      default: '', //waiting, pending, refreshing, refreshed
+      default: '', //refreshing, refreshed
     },
-    pullUpStatus: {
-      type: String,
-      default: '', //waiting, loading
+    lowerThreshold: { //距底部多远时（单位px），触发 scrolltolower 事件
+      type: Number,
+      default: 50
     },
   },
 
   watch: {
     pullDownStatus(newV) {
       if( newV === 'refreshed' ) {
-        this.downMarginTop = MARGIN_TOP
+        _pull_down_margin_top = -MARGIN_TOP
+        _speed = SPEED
+        this.$_update_down_style({ addTransition: true })
       }
     }
   },
@@ -64,14 +82,14 @@ export default {
     touchStart(e) {
       let { pageY } = e.changedTouches[0]
       _last_y = _start_y = pageY
-      _speed = SPEED
+      _scroll_elem = document.querySelector(SCROLL_ELEM)
     },
     touchMove(e) {
       let { pageY } = e.changedTouches[0]
       let offsetY = pageY - _last_y
       _last_y = pageY
       
-      return offsetY >= 0 // 每次移动的距离不超过10
+      return offsetY >= 0 // 每次move的距离不超过10px
         ? this.$_down_move(e, Math.min(offsetY, 10)) 
         : this.$_up_move(e, Math.min(offsetY, -10))
     },
@@ -80,56 +98,59 @@ export default {
       let offsetY = pageY - _start_y
       
       return offsetY >= 0 
-        ? this.$_start_pull_down_refresh(e, offsetY) 
-        : this.$_start_pull_up_load(e, offsetY)
+        ? this.$_trigger_refresh(e, offsetY) 
+        : this.$_trigger_load(e, offsetY)
     },
 
     $_down_move(e, offsetY) {
-      if(!this.pullDownStatus) return
+      // 滚动条在顶部才触发 down move
+      let isReachTop = _scroll_elem.scrollTop === 0;
 
-      this.pullDownStatus = 'pending'
-      // 滚动条在顶部时触发
-      if( document.querySelector('.a-layout-body').scrollTop === 0 ) {
-        if(this.downMarginTop <= 0) {
-          this.downMarginTop += offsetY
-        } else {
-          _speed -= 0.03
-          this.downMarginTop += offsetY * Math.max(_speed, 0.2)
-          this.pullDownStatus = 'pending'
-        }
-        //禁用ios webview 回弹效果
-        e.preventDefault()
-      }
-    },
-    $_up_move() {
-      // TODO：滚动条在底部时触发
+      if(!this.pullDownStatus || !isReachTop) return
 
-    },
-    $_start_pull_down_refresh(e, offsetY) {
-      // console.log(e)
-      if(!this.pullDownStatus) return
-
-      if(this.downMarginTop > 0 || this.pullDownStatus === 'refreshing') {
-        this.downMarginTop = 0
-        this.pullDownStatus = 'refreshing'
-        _ready_to_refresh = true
+      // 模拟下拉时先快后慢效果
+      this.pullDownStatus = 'refreshed'
+      if(_pull_down_margin_top <= -MARGIN_TOP * 2/3) {
+        _pull_down_margin_top += offsetY
       } else {
-        this.downMarginTop = MARGIN_TOP
-        this.pullDownStatus = 'waiting'
+        _speed -= 0.03
+        _pull_down_margin_top += offsetY * Math.max(_speed, 0.2)
+      }
+      this.$_update_down_style()
+      //禁用ios webview 回弹效果
+      e.preventDefault()
+    },
+    $_up_move(){},
+
+    $_trigger_refresh(e, offsetY) {
+      if(!this.pullDownStatus) return
+
+      if(_pull_down_margin_top >= 0) {
+        _pull_down_margin_top = 0
+        this.pullDownStatus = 'refreshing'
+        this.$emit('scrolltoupper')
+      } else {
+        _pull_down_margin_top = -MARGIN_TOP
       }
 
-      if(Math.abs(offsetY) >= Math.abs(MARGIN_TOP) && _ready_to_refresh ) {
-        this.$emit('start-pull-down-refresh')
-        _ready_to_refresh = false
-      }
+      this.$_update_down_style({addTransition: true})
     },
-    $_start_pull_up_load() {
-      // TODO:上拉加载...
-      this.$emit('start-pull-up-load')
+    $_trigger_load(e, offsetY) {
+      // 上拉加载...
+      let isReachBottom = WINDOW_HEIGHT + 
+        _scroll_elem.scrollTop + 
+        this.lowerThreshold >= _scroll_elem.scrollHeight;
+
+      if(!isReachBottom) return
+      this.$emit('scrolltolower')
     },
-    $_update_style(marginTop, hasTransition) {
-      let transition = hasTransition ? 'all .3s ease-in' : ''
-      this.updateStyle = `margin-top:${marginTop}px;transition:${transition}`
+    $_update_down_style({addTransition} = {}) {
+      let transition = addTransition ? 'margin-top .3s ease-in' : ''
+      this.pullDownStyle = `
+        height:${MARGIN_TOP}px;
+        margin-top:${_pull_down_margin_top}px;
+        transition:${transition}
+      `
     },
   }
 }
@@ -140,7 +161,6 @@ $rem: 1 / 18.75 * 1rem;
 
 .down-refresh {
   position: relative;
-  height: 84px;
 
   display: flex;
   justify-content:center;
@@ -163,8 +183,8 @@ $rem: 1 / 18.75 * 1rem;
   }
 
   @keyframes rotate {
-    0% {transform: rotate(0deg);transform-origin:50% 50%;}
-    100% {transform: rotate(360deg);transform-origin:50% 50%;}
+    0% {transform: rotate(60deg);transform-origin:50% 50%;}
+    100% {transform: rotate(420deg);transform-origin:50% 50%;}
   }
 }
 </style>
