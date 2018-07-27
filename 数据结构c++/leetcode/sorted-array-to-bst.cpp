9141dfb240349e869388e4cda8722b7f764872f7
CaptainLiao <15196004361@163.com>
2018-07-27 15:51:02 +0800

    add sorted-array-to-bst.cpp

--- 数据结构c++/leetcode/sorted-array-to-bst.cpp
+++ 数据结构c++/leetcode/sorted-array-to-bst.cpp
@@ -1,3 +1,7 @@
+
+//将有序数组转换为二叉搜索树
+//https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/description/
+
 typedef struct TreeNode {
   int val;
   TreeNode* left;
@@ -7,6 +11,21 @@ typedef struct TreeNode {
 };
 
 class Solution {
+private:
+  TreeNode* insert(vector<int> nums, int l, int r) {
+    if( l > r ) return NULL;
+
+    int mid = r - (r - l)/2;
+    TreeNode* root = new TreeNode(nums[mid]);
+    root->left = insert(nums, l, mid - 1);
+    root->right = insert(nums, mid + 1, r);
+    return root;
+  }
 public:
-  TreeNode* sorted
+  TreeNode* sortedArrayToBST(vector<int> &nums) {
+    int sz = nums.size();
+    if(sz == 0) return NULL;
+
+    return insert(nums, 0, sz - 1);
+  }
 }
