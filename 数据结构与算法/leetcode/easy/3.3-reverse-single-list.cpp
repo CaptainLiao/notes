0f6f7b9e8ee18914fc065f9e521cd7db3acd1d94
CaptainLiao <15196004361@163.com>
2018-08-15 09:44:47 +0800

    add 3.3-reverse-single-list.cpp

---
+++ 数据结构与算法/leetcode/easy/3.3-reverse-single-list.cpp
@@ -0,0 +1,36 @@
+/**
+反转链表
+反转一个单链表。
+
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+*/
+
+// 方法一：头插法O(n)
+ListNode* reverseList(ListNode* head) {
+  ListNode* h = head;
+  ListNode* root = NULL;
+  ListNode* next = NULL;
+
+  while (h) {
+    next = h->next;
+    h->next = root;
+    root = h;
+    h = next;
+  }
+  return root;
+}
+
+// 方法二
+ListNode* reverseList2(ListNode* head) {
+  if (head == NULL || head->next == NULL) return head;
+
+  ListNode* res = reverseList2(head->next);
+  head->next->next = head;
+  head->next = NULL;
+  return res;
+}
