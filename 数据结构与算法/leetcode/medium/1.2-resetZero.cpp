b07910252953ced237f5786f0c28ae72ad6292ef
CaptainLiao <15196004361@163.com>
2018-09-27 09:18:51 +0800

    优化resetZero.cpp

--- 数据结构与算法/leetcode/medium/1.2-resetZero.cpp
+++ 数据结构与算法/leetcode/medium/1.2-resetZero.cpp
@@ -1,19 +1,18 @@
 vector<vector<int>> resetZero(vector<vector<int>> matrix, int m, int n) {
 	vector<int> arr;
 
+
+
 	for (int i = 0; i < m; ++i) {
 		for (int j = 0; j < n; ++j) {
+			if (matrix[i][j] != 0 || arr[j] != 0) continue;
 			if (matrix[i][j] == 0) {
-				if (arr.find(j)) {
-					continue;
-				} else {
-					arr.push_back(j);
-					int k = 0;
-					int p = 0;
-					while (k < m) matrix[k++][j] = 0;
-					while (p < n) matrix[i][p++] = 0;
-					break;
-				}
+				arr.push_back(j);
+				int k = 0;
+				int p = 0;
+				while (k < m) matrix[k++][j] = 0;
+				while (p < n) matrix[i][p++] = 0;
+				break;
 			}
 		}
 	}
