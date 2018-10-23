968180f651c4f87b317819ff62504c7db8889577
CaptainLiao <15196004361@163.com>
2018-10-23 09:23:57 +0800

    leetcode/medium/: add 3.1-inorderTraversal.c

---
+++ 数据结构与算法/leetcode/medium/3.1-inorderTraversal.c
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int getTreeSize(struct TreeNode *root) {
+  if (!root) return 0;
+
+  return getTreeSize(root->left) + getTreeSize(root->right) + 1;
+}
+
+void inorder(struct TreeNode *root, int *res, int *i) {
+  if (!root) return;
+
+  inorder(root->left, res, i);
+  // why？为什么不能使用 *res = root->val; ++res; 对res进行赋值
+  res[i++] = root->val;
+  inorder(root->right, res, i);
+}
+
+int *inorderTraversal(struct TreeNode *root, int *returnSize) {
+  *returnSize = getTreeSize(root);
+  int i = 0;
+  // 向操作系统申请 *returnSize 个 int 大小的内存
+  // 用于存储最后的结果
+  int *res = (int *)malloc(*returnSize * sizeof(int));
+
+  inorder(root, res, &i);
+  return res;
+}
