8ca7b8816aba37d894ed68cd819d314be7387f24
CaptainLiao <15196004361@163.com>
2018-11-24 13:45:57 +0800

    add countDown.js

--- js/snippets/contDown.js
+++ js/snippets/contDown.js
@@ -1,29 +1,45 @@
+const RE_FORMAT = new RegExp('%([1-9]?)(.)', 'g')
+const ONE_SECOND = 1000;
+const ONE_MINUTE = ONE_SECOND * 60;
+const ONE_HOUR = ONE_MINUTE * 60;
+const ONE_DAY = ONE_HOUR * 24;
 /**
- * @param {Date} countDownTime 日期时间戳
+ * @param {RegExp} fmt 按照strftime的规则，但是多了个数字，比如 "%d"=>"02", "%1d" => "2"，目前支持最多以天为单位的倒计时 
+ * @param {time} diffTime 倒计时剩余时间 单位：毫秒
  */
-function __formatTime(countDownTime) {
-  if (countDownTime === void 0) return
-  let diffTime = countDownTime - Date.now()
-  if (diffTime < 0) return diffTime
+function format(fmt, diffTime) {
+  if (diffTime === void 0 || diffTime < 0) return diffTime
   
-  const SECOND = 1000
-  const MINUTE = 60 * SECOND
-  const HOUR = 60 * MINUTE
-  const DAY = 24 * HOUR
+  let day = parseInt(diffTime / ONE_DAY)
+  diffTime -= day * ONE_DAY
+  let h = parseInt(diffTime / ONE_HOUR)
+  diffTime -= h * ONE_HOUR
+  let m = parseInt(diffTime / ONE_MINUTE)
+  diffTime -= m * ONE_MINUTE
+  let s = parseInt(diffTime / ONE_SECOND)
 
-  let day = parseInt(diffTime / DAY)
-  diffTime -= day * DAY
-  let h = parseInt(diffTime / HOUR)
-  diffTime -= h * HOUR
-  let m = parseInt(diffTime / MINUTE)
-  diffTime -= m * MINUTE
-  let s = parseInt(diffTime / SECOND)
+  let res = fmt.replace(RE_FORMAT, function(_, digit, val) {
+    switch (val) {
+      case 'd': // 日，01-31
+        return zpad(digit || 2, day);
+      case 'H': // 时，01-24
+        return zpad(digit || 2, h);
+      case 'M': // 分，01-59
+        return zpad(digit || 2, m);
+      case 'S': // 秒，01-59
+        return zpad(digit || 2, s);
+    }
+  })
 
-  h = ('0'+h).substr(-2)
-  m = ('0'+m).substr(-2)
-  s = ('0'+s).substr(-2)
+  return res
+}
 
-  return day > 0
-    ? `${day}天 ${h}:${m}:${s}`
-    : `${h}:${m}:${s}`;
+const ZEROS = '00000000000000000000';
+function zpad(n, v) {
+  return (ZEROS + v).substr(-n)
+}
+
+export default {
+  format,
+  zpad
 }
