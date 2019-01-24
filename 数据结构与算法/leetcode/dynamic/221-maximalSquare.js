14825d081e3649248d91173c1acff6298ced471f
liaofeiyin <liaofy@133.cn>
2019-01-24 01:04:32 +0000

    update 221-maximalSquare.js

--- 数据结构与算法/leetcode/dynamic/221-maximalSquare.js
+++ 数据结构与算法/leetcode/dynamic/221-maximalSquare.js
@@ -4,15 +4,17 @@ function maximalSquare(matrix) {
   if (row === 0) return 0
   var col = matrix[0].length
 
-  var dp = Array(row + 1).fill(0).map(() => Array(col+1).fill(0))
+  var dp = Array(row).fill(0).map(() => Array(col).fill(0))
   var max = 0
 
-  for (var i = 1; i < row; ++i) {
-    for (var j = 1; j < col; ++j) {
-      if (matrix[i][j] === 1) {
+  for (var i = 0; i < row; ++i) {
+    for (var j = 0; j < col; ++j) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = matrix[i][j]
+      } else if (matrix[i][j] === 1) {
         dp[i][j] = 1 + Math.min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
-        max = Math.max(max, dp[i][j])
       }
+      max = Math.max(max, dp[i][j])
     }
   }
 
