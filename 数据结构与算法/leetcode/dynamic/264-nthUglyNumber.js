b0bbfa6b9b8bb71b25b879084235821bb18cff12
liaofeiyin <liaofy@133.cn>
2019-01-29 01:37:00 +0000

    add 264

--- 数据结构与算法/leetcode/dynamic/264-nthUglyNumber.js
+++ 数据结构与算法/leetcode/dynamic/264-nthUglyNumber.js
@@ -12,20 +12,23 @@
 1 是丑数。
 n 不超过1690。
 
+l1: 1x2, 2x2,3x2,4x2,5x2,6x2,8x2,9x2,10x2,12x2,
+l2: 1x3, 2x3,3x3,4x3,5x3,6x3,8x3,9x3,10x3,12x3,
+l3: 1x5, 2x5,3x5,4x5,5x5,6x5,8x5,9x5,10x5,12x5,
  * @param {number} n
  * @return {number}
  */
 var nthUglyNumber = function(n) {
   if (n <= 1) return n
   
-  var l1 = 2
-  var l2 = 3
-  var l3 = 5
+  var l1 = 1
+  var l2 = 1
+  var l3 = 1
   
   var count = 1
-  var res = 0
+  var res = [1]
   while (count <= n) {
-      
+      res[count] = Math.min(l1*2, l2*3, l3*5)
   }
   
   return res
