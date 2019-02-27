912bacd234668211dcbc87f5fc7b3441ea58a78a
liaofeiyin <liaofy@133.cn>
2019-02-27 01:26:38 +0000

    add leetcode/376

--- 数据结构与算法/leetcode/dynamic/376-WiggleSubsequent.java
+++ 数据结构与算法/leetcode/dynamic/376-WiggleSubsequent.java
@@ -1,5 +1,5 @@
 class Solution {
-  public int WiggleMaxLength(int[] nums) {
+  public int wiggleMaxLength(int[] nums) {
     int len = nums.length;
     if (len < 2) return len;
 
@@ -11,5 +11,26 @@ class Solution {
     }
 
     return Math.max(up, down);
+  }
+}
+
+class Solution2 {
+  public int wiggleMaxLength(int[] nums) {
+    int len = nums.length;
+    if (len <= 1) return len;
+
+    int[] temp = new int[len-1];
+    int tLen = 0;
+    for (int i = 1; i < len; ++i) {
+      if (nums[i-1] != nums[i]) temp[tlen++] = nums[i] - nums[i-1];
+    }
+
+    if (tLen <= 1) return 1;
+    
+    int count = 2;
+    for (int j = 1; j < tLen; ++j) 
+      if (temp[j] * temp[j-1] < 0) ++count;
+
+    return count;
   }
 }
