061aa67e12bb87870355f9d1c9cfc2e3afc04a5b
liaofeiyin <liaofy@133.cn>
2019-07-09 02:04:25 +0100

    add 218.minStack.java

---
+++ 数据结构与算法/leetcode/java/218.minStack.java
@@ -0,0 +1,54 @@
+package com.liaofy.spring.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinStack {
+	private List<Integer> data;
+	private Integer min = null;
+	
+	public MinStack() {
+		data = new ArrayList<Integer>();
+	}
+	
+	public void push(int x) {
+		if (min == null) min = x;
+		
+		data.add(x - min);
+
+		if (x < min) min = x;
+	}
+	public void pop() {
+		if (isEmpty()) return;
+		
+		int v = data.remove(data.size() - 1);
+		if (v < 0) min = min -v;
+	}
+	
+	public boolean isEmpty() {
+		return data.size() <= 0;
+	}
+	
+	public int top() {
+		int v = data.get(data.size() - 1);
+		if (v < 0) return min;
+		return min + v;
+	}
+	
+	public int getMin() {
+		return min;
+	}
+	
+	public static void main(String[] args) {
+		MinStack s = new MinStack();
+		s.push(1);
+        s.push(2);
+        s.push(3);
+        for (int i = 0; i < 4; ++i) {
+            if (!s.isEmpty()) {
+                System.out.println(s.top());
+            }
+        }
+
+	}
+}
