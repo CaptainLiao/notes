267c91482fcc95cb28feb99a34235df6384a08e8
liaofeiyin <liaofy@133.cn>
2019-07-18 11:07:58 +0100

    add blog/Lexical Environments

--- javascript/Design Patterns/Structural Design Patterns/Decorator.java
+++ javascript/Design Patterns/Structural Design Patterns/Decorator.java
@@ -1,6 +1,22 @@
 /**
   装饰者设计模式：
     在不改变类原有方法的前提下，丰富方法的能力
+
+  假设我们要实现两种 car：
+    首先我们创建一个含有 assemble 方法的接口 Car；
+    接着创建一个实现接口 Car 的 BaseCar 类；
+    最终，创建两个继承于 BaseCar 的 SportsCar 和 LuxuryCar;
+
+    如下：
+          Car 
+        BaseCar
+  SportsCar LuxuryCar  
+
+  如果我们想在【运行时】，得到一辆同时包含 sport 和 luxury 功能的小车，要怎么做？
+  如果还想指定装配的顺序呢？
+  如果我们有很多辆车，又该如何实现上面的功能？
+
+  要解决这些问题，我们需要使用装饰者设计模式。
  */
 
 public interface Car {
