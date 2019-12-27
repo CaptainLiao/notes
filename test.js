7e32bcebbdac1c127727e4d3b0e6ae69714f9073
liaofeiyin <liaofy@133.cn>
2019-12-27 01:15:00 +0000

    add blog/2019/12.26

--- test.js
+++ test.js
@@ -1,8 +1,23 @@
-const p = Promise.resolve();
+//const p = 42;
+const p = Promise.resolve(42)
 
-(async () => {
-  await p; console.log('after:await');
+setTimeout(() => console.log('setTimeout'))
+
+const asyncFn = (async function () {
+  await p; 
+  console.log('after:await', 1);
+  await p;
+  console.log('after:await', 2);
 })();
 
-p.then(() => console.log('tick:a'))
- .then(() => console.log('tick:b'));
+asyncFn.then(() => console.log('after:asyncFn'))
+
+Promise.resolve(p).then(() => console.log('tick:1'))
+ .then(() => console.log('tick:2'))
+ .then(() => console.log('tick:3'))
+ .then(() => console.log('tick:4')) 
+ .then(() => console.log('tick:5')) 
+ .then(() => console.log('tick:6')) 
+ .then(() => console.log('tick:7')) 
+ .then(() => console.log('tick:8')) 
+ .then(() => console.log('tick:9')) 
