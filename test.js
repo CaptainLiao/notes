cba867be1daa3cd6206398afe9ae6a043fbf4117
liaofeiyin <13296605969@163.com>
2020-10-19 17:05:51 +0800

    [add] blog/2020/10/19: 网络是怎样连接的.md

--- test.js
+++ test.js
@@ -1,23 +1,18 @@
-//const p = 42;
-const p = Promise.resolve(42)
 
-setTimeout(() => console.log('setTimeout'))
+var ftn = function ftn () {
+  console.log('old');
+  
+}
 
-const asyncFn = (async function () {
-  await p; 
-  console.log('after:await', 1);
-  await p;
-  console.log('after:await', 2);
-})();
+var b = ftn;
+console.log(b);
 
-asyncFn.then(() => console.log('after:asyncFn'))
 
-Promise.resolve(p).then(() => console.log('tick:1'))
- .then(() => console.log('tick:2'))
- .then(() => console.log('tick:3'))
- .then(() => console.log('tick:4')) 
- .then(() => console.log('tick:5')) 
- .then(() => console.log('tick:6')) 
- .then(() => console.log('tick:7')) 
- .then(() => console.log('tick:8')) 
- .then(() => console.log('tick:9')) 
+
+var ftn = function ftn () {
+  b()
+  console.log('new');
+  
+}
+
+console.log(b);
