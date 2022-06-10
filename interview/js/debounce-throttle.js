c1c023bf28589ed9ff9698b1c5b05a9ce55e0c3d
liaofeiyin <13296605969@163.com>
2022-06-10 17:21:39 +0800

    feat(blog): 使用service worker还原用户现场

--- interview/js/debounce-throttle.js
+++ interview/js/debounce-throttle.js
@@ -21,3 +21,14 @@ function throttle(fn, time) {
     }
   }
 }
+
+function throttle2(fn, delay) {
+  let lastTime = 0
+  return function(...args) {
+    const now = Date.now()
+    if (now - lastTime >= delay) {
+      fn.call(this, ...args)
+      lastTime = now
+    }
+  }
+}
